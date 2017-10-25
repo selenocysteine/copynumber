8d5100f3eaa8fdd82faae30ac98faf4ac75c7518
Chiara Cotroneo <chaira.cotroneo@ucdconnect.ie>
2017-10-25 15:35:44 +0100

    interpolate.pcf.r : fixed a bug which led the function to crash when there is no observations for a sample on one or more chromosomes.

--- R/interpolate.pcf.r
+++ R/interpolate.pcf.r
@@ -1,9 +1,9 @@
 ####################################################################
-## Author: Gro Nilsen, Knut Liestøl and Ole Christian Lingjærde.
+## Author: Gro Nilsen, Knut Liestï¿½l and Ole Christian Lingjï¿½rde.
 ## Maintainer: Gro Nilsen <gronilse@ifi.uio.no>
 ## License: Artistic 2.0
 ## Part of the copynumber package
-## Reference: Nilsen and Liestøl et al. (2012), BMC Genomics
+## Reference: Nilsen and Liestï¿½l et al. (2012), BMC Genomics
 ####################################################################
 
 ### Requires:
@@ -11,12 +11,12 @@
 
 #Function with interpolates pcf-segments
 interpolate.pcf = function(segments, x) {
-  
+
   #Make sure segments is a data frame
   segments <- pullOutContent(res=segments,what="segments")
-  
+
   usamp = unique(segments$sampleID)
-  nsamp = length(usamp) 
+  nsamp = length(usamp)
   chrom = unique(x[,1])
   z = data.frame(cbind(x[,c(1:2)],matrix(0,nrow(x),nsamp)))
   #z = data.frame(x[,c(1,2)], matrix(0, nrow(x), nsamp))
@@ -24,16 +24,18 @@ interpolate.pcf = function(segments, x) {
   for (i in 1:nsamp) {
     for (j in 1:length(chrom)) {
       fitij = segments[segments$sampleID==usamp[i] & segments$chrom==chrom[j],]
-      v = (c(fitij$start.pos[-1],10^9)+fitij$end.pos)/2
-      xj = x[x[,1]==chrom[j],2]
-      kj = rep(0,length(xj))
-      for (k in rev(1:length(v))) {
-        kj[xj <= v[k]] = k
+      # If clause: fixes those cases where the dataset is incomplete and
+      # there isn't data for each sample for each chromosome:
+      if(nrow(fitij) > 0){
+        v = (c(fitij$start.pos[-1],10^9)+fitij$end.pos)/2
+        xj = x[x[,1]==chrom[j],2]
+        kj = rep(0,length(xj))
+        for (k in rev(1:length(v))) {
+          kj[xj <= v[k]] = k
+        }
+        z[z$chr==chrom[j],2+i] = fitij$mean[kj]
       }
-      z[z$chr==chrom[j],2+i] = fitij$mean[kj]
     }
   }
   return(z)
 }
-
-
